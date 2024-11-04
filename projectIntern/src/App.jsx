e1382191facee3219ac7ac3822ad2a0de758890e
M42UJO <khamso0092@gmail.com>
2024-11-04 13:33:49 +0700

    login

--- projectIntern/src/App.jsx
+++ projectIntern/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Example from './components/login.jsx'
 
 function App() {
   return (
+    <>
     <div className='mt-5'>App</div>
+    <Example />
+    </>
   )
 }
 
