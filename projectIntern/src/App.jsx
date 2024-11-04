056274d31e227e99bca9ecda24d033568f62ac9c
Gumpwsw <wasawatsutthitham@gmail.com>
2024-11-04 14:23:17 +0700

    bglogin

--- projectIntern/src/App.jsx
+++ projectIntern/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Example from './components/login.jsx'
-
+import ExampleL from "./components/BgLogin.jsx";
 function App() {
   return (
     <>
-    <Example />
-    
+    <ExampleL />
     </>
   )
 }
---
+++ projectIntern/src/components/BgLogin.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './img/logo.png';
+
+export default function ExampleL() {
+    return (
+        <>
+        
+            <div style={{ backgroundColor: '#11111D', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div style={{ display: 'flex', gap: '30rem' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <img src={logo} alt="Logo" style={{ width: '389x' }} />
+                        <h2 style={{ color: '#FFFFFF', fontWeight: 'bold', marginTop: '1rem', textAlign: 'center' }}>Data Collection System</h2>
+                        <p style={{ color: '#FFFFFF', textAlign: 'center' }}>business value co.td</p>
+                    </div>
+
+                    <div style={{ backgroundColor: '#FFFFFF', padding: '2rem', borderRadius: '8px', width: '300px' }}>
+                        <h2 style={{ textAlign: 'center', marginBottom: '1rem' }}>Login BOXXXX</h2>
+                        <form>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
